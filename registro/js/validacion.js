ba78a149d37b34189895243ef1743bd63c8cbad4
GabrielaEva <ggab431@gmail.com>
2025-08-13 19:14:57 -0300

    agregue lo de la contraseña mayor a 6 caracteres

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+let botonRegistro = document.getElementById("regBtn");
+botonRegistro.addEventListener("click", function(event) {
+    event.preventDefault();
+    let password = document.getElementById("password1").value;
+    if (password.length < 6) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+});
