1680864ccd9eea995e2d292e7698618b5d77cb22
Axel <axelbravo28885@gmail.com>
2025-08-13 19:18:35 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const registro = document.getElementById("registroForm");
+
+registro.addEventListener("submit", function (event) {
+  const password = document.getElementById("password1").value;
+  const confirmPassword = document.getElementById("password2").value;
+
+  if (password !== confirmPassword) {
+    event.preventDefault();
+    showAlertError();
+  }
+});
