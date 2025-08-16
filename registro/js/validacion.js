3f75946d6de40e520896a62b16300c9afc981019
Axel <axelbravo28885@gmail.com>
2025-08-16 19:29:11 -0300

    Delete registro directory

--- registro/js/validacion.js
+++
@@ -1,7 +0,0 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
